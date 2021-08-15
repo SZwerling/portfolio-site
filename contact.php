086d5ae9a886ce0234b326e0e7dc9307035f3be1
Slim <sazwerling@gmail.com>
2021-08-15 14:40:07 -0500

    different php

--- contact.php
+++ contact.php
@@ -1,19 +1,10 @@
-<?php 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message']
-
-    $email_from = 'portfolio site';
-    $email_subject = 'new message from portfolio site';
-    $email_body = "Name: $name.\n".
-                  "email: $email.\n".
-                  "message: $message.\n";
-
-    $to = "sazwerling@gmail.com";
-    $headers = "From: $email_from \r\n";
-    $headers = "Reply-To: $email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("location: index.html");
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "sazwerling@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
